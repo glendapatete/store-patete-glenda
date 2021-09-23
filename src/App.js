e7c244e1f92677956dcd441af84294a503a361a0
Glenda Patete <glendapatete@MacBook-Pro-de-Glenda.local>
2021-09-23 11:17:44 -0300

    fix router HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Redeem from "./Pages/Redeem";
 function App() {
